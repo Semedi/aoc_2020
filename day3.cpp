be086fed6db201ad1285dca7b1eb15b5c4c9f50f
Sergio Semedi <sergio.semedi@gmail.com>
2020-12-03 14:29:43 +0100

    part 2 completed

--- day3.cpp
+++ day3.cpp
@@ -1,9 +1,15 @@
 #include<fstream>
 #include <iostream>
 #include <string>
+#include <tuple>
+#include <vector>
+using namespace std;
 
 #define WIDTH 31
 #define HEIGHT 323
+#define X 0
+#define Y 1
+#define T 2
 
 int tile(int x, int y)
 {
@@ -13,21 +19,48 @@ int tile(int x, int y)
 int main(int argc, char*argv[])
 {
   char c;
-  std::ifstream in("input/day3.txt");
+  ifstream in("input/day3.txt");
   bool map[WIDTH * HEIGHT];
-  int i = 0;
+  int i = 0, checked = 0;
+  long m = 1;
 
   while(in.get(c)){
       if (c != '\n')
           map[i++] = (c == '#');
   }
 
-  int trees = 0, x = 0;
+  // X Y T
+  vector<tuple<int,int, bool>> dir{{1,1,0},{3,1,0},{5,1,0},{7,1,0},{1,2,0}}; 
+  vector<tuple<int,int,int>> slope{{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}};
 
-  for (int y = 1; y < HEIGHT; y++){
-      trees += map[tile(x+=3, y)];
+  while (checked != slope.size()){
+      for (int s = 0; s < slope.size(); s++){
+        if (get<T>(dir[s])) continue;
+
+        int x = get<X>(slope[s]) += get<X>(dir[s]);
+        int y = get<Y>(slope[s]) += get<Y>(dir[s]);
+        get<T>(dir[s]) = (y >= HEIGHT);
+        if (y >= HEIGHT){
+            checked++;
+            cout << "slope:" << s << " trees encountered: " << get<T>(slope[s]) << endl;
+            m *= get<T>(slope[s]);
+        }
+
+        get<T>(slope[s]) += map[tile(x,y)];
+
+      }
   }
-  std::cout << "trees encountered: " << trees << std::endl;
+
+  cout << "result: " << m << endl;
+
+  return 0;
+
+}
+  //x = 0;
+  //for (int y = 1; y < HEIGHT; y++)
+  //    trees += map[tile(x+=3, y)];
+  //
+  //cout << "trees encountered: " << trees << endl;
 
 
   //for(int i = 0; i < WIDTH*HEIGHT; i++){
@@ -36,11 +69,3 @@ int main(int argc, char*argv[])
 
   //    std::cout << map[i];
   //}
-
-
-
-
-
-  return 0;
-
-}
