df87806d180831de0a2d51757fd0e3a41abdd31e
Sergio Semedi <sergio.semedi@gmail.com>
2020-12-06 12:15:23 +0100

    day 5 part 1 completed

--- day5.cpp
+++ day5.cpp
@@ -2,51 +2,38 @@
 #include <fstream>
 
 using namespace std;
-int row(string seat, int b, int u){
+int get_seat(string seat, int b, int u){
     if (b == u)
         return b;
 
     char letter = seat.front();
 
-    if (letter == 'F')
+    if (letter == 'F' || letter == 'L')
         u = (u-b)/2 + b;
 
-    if (letter == 'B')
+    if (letter == 'B' || letter == 'R')
         b = (u-b)/2 + 1 + b;
 
-    return row(seat.substr(1, seat.length()), b, u);
-}
-
-int column(string seat, int b, int u){
-    if (b == u)
-        return b;
-
-    char letter = seat.front();
-
-    if (letter == 'L')
-        u = (u-b)/2 + b;
-
-    if (letter == 'R')
-        b = (u-b)/2 + 1 + b;
-
-    return column(seat.substr(1, seat.length()), b, u);
+    return get_seat(seat.substr(1, seat.length()), b, u);
 }
 
 int main ()
 {
     string str; 
     ifstream file("input/day5.txt");
-    // read input
-    while (getline(file, str)) {
-        cout << str << endl;
-        string str = "FBFBBFFRLR";
-        //cout << str.substr(0, 7) << endl;
-        //cout << 127/2 << endl;
-        cout << row(str.substr(0,str.length()-3), 0, 127) << endl; 
-        cout << column(str.substr(str.length()-3), 0, 7) << endl;
 
-        break;
+
+    int maxid = 0;
+    while (getline(file, str)) {
+        int row    = get_seat(str.substr(0,str.length()-3), 0, 127); 
+        int column = get_seat(str.substr(str.length()-3), 0, 7);
+
+        int id = row * 8 + column; 
+
+        maxid = (id > maxid) ? id: maxid;
     }
+
+    cout << "max id: " << maxid << endl;
 
 
 }
