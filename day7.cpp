a3d9a4937e376cb2e01df2dc170929494569589f
Sergio Semedi <sergio.semedi@gmail.com>
2020-12-08 19:59:40 +0100

    new regexp parse input

--- day7.cpp
+++ day7.cpp
@@ -1,61 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <regex>
+#include <unordered_map>
 
+typedef std::unordered_map<std::string, int> tbag;
 using namespace std;
-std::vector<std::string> split(std::string str, std::string sep){
-    char* cstr = const_cast<char*>(str.c_str());
-    char* current;
-
-    std::vector<std::string> arr;
-
-    current=strtok(cstr, sep.c_str());
-
-    while(current!=NULL){
-        arr.push_back(current);
-        current=strtok(NULL,sep.c_str());
-    }
-
-    return arr;
-}
-
-int contained(string phrase)
-{
-
-    auto bags_vec = split(phrase, ",");
-
-    regex rgx("^ (\\d) ([a-z]+ [a-z]+) bags?$");
-    for (auto& b: bags_vec){
-        std::smatch matches;
-        if (regex_search(b, matches, rgx)) {
-            cout << b << endl;
-        }
-        else {
-            cout << "match error asdsad" << endl;
-        }
-    }
-
-    return 0;
-}
 
 int main ()
 {
     std::string str; 
     std::ifstream file("input/day7.txt");
-    regex rgx("^([a-z]+ [a-z]+) bags contain((?: \\d [a-z]+ [a-z]+ bags?,?)*)\\.$");
+    regex parent_rgx("^(\\w+ \\w+).*$");
+    regex child_rgx("(\\d+) (\\w+ \\w+)");
+    unordered_map<string, tbag> bags;
 
     while (std::getline(file, str)) {
 
-        std::smatch matches;
-        if (regex_search(str, matches, rgx)) {
+        smatch matches;
+        if (regex_search(str, matches, parent_rgx)) {
 
-            cout << matches[1].str() << endl; 
-            contained(matches[2].str());
+            auto parent = matches[1].str();
 
+            auto rbegin = sregex_iterator(str.begin(), str.end(), child_rgx);
+            auto rend   = sregex_iterator();
+            for (auto i = rbegin; i != rend; ++i){
+                smatch match = *i;
+                cout << match.str() << endl;
+            }
         }
         else {
             cout << "match error!" << endl;
-
             return 1;
         }
 
