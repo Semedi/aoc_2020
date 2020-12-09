140cdf647b9b2af67c5b0fe1a38fc68fb1d00b08
Sergio Semedi <sergio.semedi@gmail.com>
2020-12-09 09:00:25 +0100

    day7 part2 finished!

--- day7.cpp
+++ day7.cpp
@@ -4,7 +4,42 @@
 #include <unordered_map>
 
 typedef std::unordered_map<std::string, int> tbag;
+typedef std::unique_ptr<tbag> ptr_bag;
 using namespace std;
+
+string target_bag = "shiny gold";
+
+const bool holds(const unordered_map<string, ptr_bag> &bags, string outer)
+{
+    // bag is empty
+    if (bags.at(outer)->size() == 0)
+        return false;
+
+    auto exists = bags.at(outer)->find(target_bag) != bags.at(outer)->end();
+
+    // target_bag exists exists in child
+    if (exists)
+        return true;
+
+    for (auto i = bags.at(outer)->begin(); i != bags.at(outer)->end(); ++i){
+        exists |= holds(bags, i->first);
+    }
+
+    return exists;
+}
+
+int size(const unordered_map<string, ptr_bag> &bags, string outer)
+{
+    int n = outer != target_bag;
+    if (bags.at(outer)->size() == 0)
+        return n;
+
+    for (auto i = bags.at(outer)->begin(); i != bags.at(outer)->end(); ++i){
+        n += (i->second * size(bags, i->first));
+    }
+
+    return n;
+}
 
 int main ()
 {
@@ -12,7 +47,7 @@ int main ()
     std::ifstream file("input/day7.txt");
     regex parent_rgx("^(\\w+ \\w+).*$");
     regex child_rgx("(\\d+) (\\w+ \\w+)");
-    unordered_map<string, tbag> bags;
+    unordered_map<string, ptr_bag> bags;
 
     while (std::getline(file, str)) {
 
@@ -21,20 +56,32 @@ int main ()
 
             auto parent = matches[1].str();
 
+            ptr_bag child(new tbag());
             auto rbegin = sregex_iterator(str.begin(), str.end(), child_rgx);
             auto rend   = sregex_iterator();
             for (auto i = rbegin; i != rend; ++i){
                 smatch match = *i;
-                cout << match.str() << endl;
+                child->insert(pair<string, int>(match[2].str(), stoi(match[1].str())));
             }
+
+            bags.insert(pair<string, ptr_bag>(parent, move(child)));
         }
         else {
-            cout << "match error!" << endl;
+            cout << "Pa tu CASA!" << endl;
             return 1;
         }
-
-        break;
     }
+
+    // part 1
+    int count = 0;
+    for (const auto& b: bags){
+        count += holds(bags, b.first);
+    }
+
+    cout << count << " bags can hold shiny gold" << endl;
+
+    //part 2
+    cout << size(bags, target_bag) << " bags inside shiny gold" << endl;
 
     return 0;
 }
