4a8b7620cafb76c3e452c8ec2dacfac062ab9071
Sergio Semedi <sergio.semedi@gmail.com>
2020-12-12 22:01:36 +0100

    day 10

--- day10.cpp
+++ day10.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <set>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
+
+
+unordered_map<int, int> DP;
+vector<int> xs;
+
+int dp(int i)
+{
+    if (i == xs.size()-1)
+        return 1;
+
+    if (DP.find(i) != DP.end())
+        return DP.at(i);
+
+    auto ans = 0;
+    for (int j = i + 1; j < xs.size(); ++j){
+        if (xs[j] - xs[i] <= 3)
+            ans += dp(j);
+    }
+    DP[i] = ans;
+
+    return ans;
+}
+
 int main () {
     std::ifstream file("input/day10.txt");
     std::string str; 
@@ -16,7 +41,6 @@ int main () {
     int n1 = 1, n3 = 1;
     for (auto it = ++(next(v.begin())); it != v.end(); ++it){
         int diff = *(it) - *prev(it);
-        cout << *it << endl;
 
         if (diff == 1) n1++;
         if (diff == 3) n3++;
@@ -25,6 +49,9 @@ int main () {
     v.insert(*(--v.end())+3);
     cout << "part1: " << n1*n3 << endl;
     cout << "n3: " << n1 << " n3:" << n3 << endl;
+
+    copy(v.begin(), v.end(), back_inserter(xs));
+    cout << "part2: " << dp(0) << endl;
 
 
     return 0;
